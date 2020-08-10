ee893ccfe7010b8d590fd9c6404a21447951deba
stefan.lines <slines@vld568.cmpd1.metoffice.gov.uk>
2020-08-10 10:16:57 +0100

    Updated people list.

--- teaminfo.py
+++ teaminfo.py
@@ -11,99 +11,14 @@ def info_Stefan():
     print("International Spy")
     print("That likes 3.1415926")
 
-def info_Tom():
-    print("Thomas Crocker")
-    print("== Scientist")
-    print("== thomas.crocker@metoffice.gov.uk")
-    print("== Climate Information for International Development")
-    print("Needs a haircut")
-
 def info_Laura():
-    print("TODO")
     print("Laura is quite slow at git")
-
-def info_Grace():
-    print("Grace Redmond")
-    print("I enjoy cheese")
-    print("I do not enjoy marmite")
-    print(":D")
-
-def info_Katy():
-    print("Katy Richardson")
-    print("== Senior Scientist")
-    print("== katy.richardson@metoffice.gov.uk")
-    print("== Climate Information and International Development")
-    print("== It's Friday")
-    print("== Happy Weekend!")
-
-def info_Scott():
-    print("Scott Burgan")
-    print("scott.burgan@metoffice.gov.uk")
-    print("New starter - foundation scientist")
-    print("Climate Information and International Development")
-    print("TODO")
-
-def info_Sarah():
-    print("TODO")
-
-def info_Erasmo():
-    print("Erasmo Buonomo")
-    print("== Regional Climate Modeller")
-    print("== erasmo.buonomo@metoffice.gov.uk")
-    print("== Climate Information and International Development")
-
-def info_Cathryn():
-    print("Cathryn Fox")
-    print("== Scientist")
-    print("== cathryn.fox@metoffice.gov.uk")
-    print("== Climate Information for International Development")
-    print("== Hello git fans")
-    print("== Happy Friday!")
-
-def info_Alex():
-    print("This is a complication to making commitment harder from Alex C-Clay")
-    print("Adding line number two!")
-
-def info_Richard():
-    print("TODO")
-
-def info_Catherine():
-    print("Catherine Bradshaw is busy")
-    print("Can I do it later?")
-    print("Today is taken up with a git workshop")
-    print("I will do it next month.")
 
 def info_Nick():
     print("Nick Savage")
     print("== General nuisance")
     print("== nicholas.savage@metoffice.gov.uk")
     print("== Climate Information and International Development (CIID)")
-
-def info_Saeed():
-    print("TODO")
-
-def info_James():
-    print("TODO")
-
-def info_Bernd():
-    print("TODO")
-    print(" Bernd was here!")
-    print("== Senior Scientist")
-    print("== bernd.becker@metoffice.gov.uk")
-    print("== Climate Information and International Development")
-
-    print("== New Cyclist!")
-
-
-def info_Jemma():
-    print("TODO")
-    print("Jemma Davie")
-    print("Scientist")
-    print("Climate Security")
-    print("International Applied Science")
-
-def info_Edward():
-    print("TODO")
 
 def info_richard():
     print("TODO")
